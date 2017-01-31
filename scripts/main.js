81a02199dcfc7819a283364a1c77a45b953ef549
Connie Rosendale <connie.rosendale@incisivemedia.com>
2017-01-31 15:40:36 +0000

    Adding initial files

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#type-heading').typeIt({
+		strings: ["This is a great string.", "But here is a better one."],
+		speed: 50,
+		breakLines: false,
+		autoStart: false
+	});
+});
